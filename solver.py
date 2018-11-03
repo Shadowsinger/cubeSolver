7c6a1236473eb404d02f96047d00a42cb5e485d9
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-11-03 14:26:47 -0400

    Fixed M/Mi bug, also cleaned up code

--- solver.py
+++ solver.py
@@ -1,109 +1,21 @@
 
 # 2x2 Rubik's Cube Solver
-# Authors: Alek and Max
-# https://www.youcandothecube.com/solve-it/2-x-2-solution
+# Authors: Alek, Max, Kevin
 
 import numpy as np
 from pprint import pprint
-#  import requests
+from commands.moves import moves
+from commands.algorithms import algs
+from commands.tools import rotate, doFormula, COLORS, perfectCube
 
 cube = []
-COLORS = ['G', 'Y', 'B', 'W', 'R', 'O']
 for color in COLORS:
     cube.append(np.array([[color, color, color],[color, color, color],[color, color, color]]))
 
-# indpairs specifies what side goes to what side
-# ind flips is the indices on those sides to flip
-# ie for R turn the first column of 1 into the first column of 0
-"""
-   [0]
-[5][1][4]
-   [2]
-   [3]
-On all faces this is what it looks like in terms of indices
-[00 01 02]
-[10 11 12]
-[20 21 22]
-"""
-moves = {
-        "M":  {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":([0,1,2],1)},
-        "Mi": {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":([0,1,2],1)},
-        "R":  {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":([0,1,2],2)},
-        "Ri": {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":([0,1,2],2)},
-        "L":  {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":([0,1,2],0)},
-        "Li": {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":([0,1,2],0)},
-        "U":  {"indpairs":[(0,4),(5,0),(2,5),(4,2)], "indflips":(0)},
-        "Ui": {"indpairs":[(4,0),(0,5),(5,2),(2,4)], "indflips":(0)},
-        "D":  {"indpairs":[(4,0),(0,5),(5,2),(2,4)], "indflips":(2)},
-        "Di": {"indpairs":[(0,4),(5,0),(2,5),(4,2)], "indflips":(2)},
-
-        "F":  {"indpairs":[(1,4),(4,3),(3,5),(5,1)], "indflips":(2)},
-        "Fi": {"indpairs":[(4,1),(3,4),(5,3),(1,5)], "indflips":(2)},
-        "B":  {"indpairs":[(4,1),(3,4),(5,3),(1,5)], "indflips":(0)},
-        "Bi": {"indpairs":[(1,4),(4,3),(3,5),(5,1)], "indflips":(0)},
-        }
-
-algs = {}
-algs["Fish_1"] = ['R','U','Ri','U','R','U','U','Ri']
-algs["Fish_2"] = algs["Fish_1"]+['U']+['U']+algs["Fish_1"]
-algs["The_Cat(Website case 5)"] = algs["Fish_1"]+['U']+algs["Fish_2"]
-algs["Diagonal(Website case 7)"] = algs["Fish_1"]+algs["Fish_2"]
-algs["The_double_sides(Website case 2)"] = algs["Fish_1"]+algs["Fish_1"]
-algs["(Website case 3)"] = algs["Fish_1"]+['U']+algs["Fish_1"]
-algs["(Webiste case 6)"] = algs["Fish_1"]+['Ui']+["Fish_2"]
-algs["Diagonal PLL"] = ['Ri','F','Ri','B','B','R','Fi','Ri','B','B','R','R']
-algs["Parralel PLL switch"] = algs["Diagonal PLL"]+['Ui']
-
-algs['upRotation'] =   ['R', 'Li']
-algs['sideRotation'] = ['U', 'Di']
-algs['frontRotation'] = ['F', 'Bi']
-
-tmp = []
-for i in range(len(COLORS)):
-    for row in range(3): 
-        for col in range(3): 
-            tmp.append(([i, [row, col]], COLORS[i], True))
-
-def rotate(oldcube, command):
-    newcube = [np.copy(oldcubei) for oldcubei in oldcube]
-
-    if command == "F": 
-        newcube[4][:,0] = oldcube[1][2]
-        newcube[3][0] = oldcube[4][:,0]
-        newcube[5][:,2] = oldcube[3][0]
-        newcube[1][2] = oldcube[5][:,2]
-    elif command == "Fi": 
-        newcube[5][:,2] = oldcube[1][2]
-        newcube[3][0] = oldcube[5][:,2]
-        newcube[4][:,0] = oldcube[3][0]
-        newcube[1][2] = oldcube[4][:,0]
-    elif command== "B": 
-        newcube[1][0] = oldcube[4][:,2]
-        newcube[4][:,2] = oldcube[3][2]
-        newcube[3][2] = oldcube[5][:,0]
-        newcube[5][:,0] = oldcube[1][0]
-    elif command== "Bi": 
-        newcube[4][:,2] = oldcube[1][0]
-        newcube[3][2] = oldcube[4][:,2]
-        newcube[5][:,0] = oldcube[3][2]
-        newcube[1][0] = oldcube[5][:,0]
-    
-    else: 
-        for indpair in moves[command]["indpairs"]:
-            newcube[indpair[1]][moves[command]["indflips"]] = oldcube[indpair[0]][moves[command]["indflips"]]
-    return newcube
-
-# Fix this to take Fi
-def doFormula(oldcube, formula):
-    for move in formula.split():
-        oldcube = rotate(oldcube, move)
-    return oldcube
-
-
+cube = doFormula(cube, "X")
 pprint(cube)
-print("--------")
-cube = doFormula(cube, "Fi B")
-#  cube = rotate(cube, "F")
-cube = rotate(cube, "B")
-pprint(cube)
+
+states = {
+    "Fish_1": [([1, [1,0]], "Y", True)]
+}
 
